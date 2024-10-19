a8b48ffdd74f67aa3035794523f7f07929bf05be
nurrrhmanf <nurrahmanfadilah44@gmail.com>
2024-10-20 02:20:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById("form-kontak").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Terima kasih sudah menghubungi kami!");
+});
